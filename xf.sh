1f0bd1dd0879ca5f457655d1916e98e8e44a84d2
GlimmerMonth <107028738+GlimmerMonth@users.noreply.github.com>
2022-06-09 10:59:56 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "808df298-ca62-aaaf-8eef-54e64acf3cf1",
                         "alterId": 0
                     }
                 ],
